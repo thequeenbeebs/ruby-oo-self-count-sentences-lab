0732f545f5aa65aca95fcd5ed90879d848fa5ed5
Blaire Baker <bakerblaire@gmail.com>
2020-11-17 18:14:10 -0600

    complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_count = 0
+    words = self.split(' ')
+    words.each do |index|
+      if index.sentence? || index.question? || index.exclamation?
+        sentence_count += 1
+      end
+    end
+    sentence_count
   end
 end
